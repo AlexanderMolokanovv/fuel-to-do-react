012f05968eb1af24d618e9c3d593f1d41515fa83
AlexanderMolokanov <91531787+AlexanderMolokanov@users.noreply.github.com>
2025-08-15 12:41:33 +0800

    Add updated README with fixed formatting and MIT License

--- dump-src.sh
+++ dump-src.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-([ -d . ] && find . -maxdepth 1 -type f -name "*.json" ! -name "package-lock.json" -printf "\nФайл: %p\n" -exec cat {} \; -printf "\n") && find ./src -type f \( -name "*.css" -o -name "*.jsx" -o -name "*.js" \) -printf "\nФайл: %p\n" -exec cat {} \; -printf "\n" > src.txt
+([ -d . ] && find . -maxdepth 1 -type f -name "*.json" ! -name "package-lock.json" ! -name "Топливный подбор" ! -name "Топливный подбор3" ! -name "*.pdf" ! -name "*.jpg" ! -name "*.fig" -printf "\nФайл: %p\n" -exec cat {} \; -printf "\n") && find ./src -type f \( -name "*.css" -o -name "*.jsx" -o -name "*.js" \) ! -name "Топливный подбор" ! -name "Топливный подбор3" ! -name "*.pdf" ! -name "*.jpg" ! -name "*.fig" -printf "\nФайл: %p\n" -exec cat {} \; -printf "\n" > src.txt
